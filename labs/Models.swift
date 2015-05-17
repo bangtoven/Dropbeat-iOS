d5c9d8abedb1a2aed6aec0feeee2a689e06c8cbc
daftshady <daftonshady@gmail.com>
2015-05-17 21:24:03 +0900

    swifty

---
+++ labs/Models.swift
@@ -0,0 +1,13 @@
+//
+//  Models.swift
+//  labs
+//
+//  Created by Park Il Su on 2015. 5. 17..
+//  Copyright (c) 2015년 dropbeat. All rights reserved.
+//
+
+import Foundation
+
+class Parser {
+    
+}

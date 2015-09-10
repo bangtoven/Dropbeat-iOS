94d41c2eae166501c006183b66409960bd39bbf3
Jungho Bang <me@bangtoven.com>
2015-09-10 22:41:10 +0900

    playground target added

---
+++ playground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  playground
+//
+//  Created by Jungho Bang on 2015. 9. 10..
+//  Copyright (c) 2015년 dropbeat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
---
+++ playground/playground-bridging.h
@@ -0,0 +1,29 @@
+//
+//  playground-bridging.h
+//  labs
+//
+//  Created by Jungho Bang on 2015. 9. 10..
+//  Copyright (c) 2015년 dropbeat. All rights reserved.
+//
+
+#ifndef labs_playground_bridging_h
+#define labs_playground_bridging_h
+
+#import "GAI.h"
+#import "GAIDictionaryBuilder.h"
+#import "GAIEcommerceFields.h"
+#import "GAIEcommerceProduct.h"
+#import "GAIEcommerceProductAction.h"
+#import "GAIEcommercePromotion.h"
+#import "GAIFields.h"
+#import "GAILogger.h"
+#import "GAITrackedViewController.h"
+#import "GAITracker.h"
+
+#import "MBProgressHUD.h"
+
+#import "Reachability.h"
+
+#import "CommonCrypto/CommonCrypto.h"
+
+#endif

6ab48173970ac0273b967f6a028ce46bdae04e5a
Jungho Bang <me@bangtoven.com>
2015-09-12 05:33:41 +0900

    playground project for user model

--- playground/ViewController.swift
+++ playground/ViewController.swift
@@ -9,11 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        Requests.emailSignin("jungho@jungho.com", password: "jungho") { (req:NSURLRequest, resp:NSHTTPURLResponse?, result:AnyObject?, error:NSError?) -> Void in
+            println("signed in")
+            
+//            Account.getAccountWithCompletionHandler({ (account:Account?, error) -> Void in
+//                println(account)
+//            })
+            
+        }
+        
     }
     
-}
+    @IBAction func userBtnTapped(sender: AnyObject) {
+        self.requestUserInfo("teksu")
+    }
+    
+    @IBAction func artistBtnTapped(sender: AnyObject) {
+        self.requestUserInfo("oliver-heldens")
+    }
 
+    @IBAction func channelBtnTapped(sender: AnyObject) {
+        self.requestUserInfo("liquicity")
+    }
+    
+    func requestUserInfo(string: String!) {
+        let baseUrl = "http://spark.coroutine.io/api/v1/resolve/?url=spark.coroutine.io/r/"
+        var requestUrl = baseUrl + string
+        Requests.sendGet(requestUrl, params: nil, auth: false, background: false) { (req, resp, result, error) -> Void in
+            var jsonData = JSON(result!)["data"]
+            var userType = jsonData["user_type"]
+            
+            switch userType {
+            case "user":
+                var user: User = User.parseUser(result!,key:"data",secondKey: "user")
+                break
+            case "artist":
+                
+                
+                break
+            case "channel":
+                var channel: Channel! = Channel.parseChannel(result!,key:"data",secondKey: "user")
+                
+                break
+            default:
+                UIAlertView(title: "망했어요", message: result?.stringValue, delegate: nil, cancelButtonTitle: "알았다.").show()
+            }
+        }
+    }
+}

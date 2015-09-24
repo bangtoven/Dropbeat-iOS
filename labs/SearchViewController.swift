0b2662833817da642e4bf62174a0c31791f16683
Jungho Bang <me@bangtoven.com>
2015-09-25 08:22:51 +0900

    search 에서 뒤로가기에 back 안보이게.

--- labs/SearchViewController.swift
+++ labs/SearchViewController.swift
@@ -28,6 +28,7 @@ class SearchViewController: AddableTrackListViewController,
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title:" ", style:.Plain, target:nil, action:nil)
         
         // Search bar initialize as code 
         // cause in storyboard cannot set searchbar with full width
